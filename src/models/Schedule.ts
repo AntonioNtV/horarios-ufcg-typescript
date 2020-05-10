feb959056942cf815cc13c459253e71eb5726e0f
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2020-05-10 18:56:22 -0300

    Refactoring Schedule Schema

--- src/models/Schedule.ts
+++ src/models/Schedule.ts
@@ -1,26 +1,32 @@
 import mongoose, { Document, Schema } from 'mongoose'
 
 type Schedule = Document & {
+    code: string;
     name: string;
     classroom: string;
     discipline: string;
     class: string;
-    teacher: string;
     category: string;
-    newPPCperiod: string;
-    oldPPCperiod: string;
     day: string;
-    hour:string
+    hour:string;
+    period: string;
     completeSchedule(): object
 }
 
 const ScheduleSchema = new Schema(
   {
+    code: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      unique: false,
+      required: true
+    },
     name: {
       type: String,
       lowercase: true,
       trim: true,
-      unique: true,
+      unique: false,
       required: true
     },
     classroom: {
@@ -34,7 +40,7 @@ const ScheduleSchema = new Schema(
       type: String,
       lowercase: true,
       trim: true,
-      unique: true,
+      unique: false,
       required: true
     },
     class: {
@@ -44,28 +50,7 @@ const ScheduleSchema = new Schema(
       unique: false,
       required: true
     },
-    teacher: {
-      type: String,
-      lowercase: true,
-      trim: true,
-      unique: false,
-      required: true
-    },
     category: {
-      type: String,
-      lowercase: true,
-      trim: true,
-      unique: false,
-      required: true
-    },
-    newPPCperiod: {
-      type: String,
-      lowercase: true,
-      trim: true,
-      unique: false,
-      required: true
-    },
-    oldPPCperiod: {
       type: String,
       lowercase: true,
       trim: true,
@@ -85,6 +70,13 @@ const ScheduleSchema = new Schema(
       trim: true,
       unique: false,
       required: true
+    },
+    period: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      unique: false,
+      required: true
     }
   }
 )
@@ -92,17 +84,16 @@ const ScheduleSchema = new Schema(
 ScheduleSchema.methods.completeSchedule = function (): object {
   return {
     name: this.name,
-    classroom: this.classrom,
+    code: this.code,
+    classroom: this.classroom,
     discipline: this.discipline,
     class: this.class,
-    teacher: this.teacher,
     category: this.category,
-    newPPCperiod: this.newPPCperiod,
-    oldPPCperiod: this.oldPPCperiod,
     schedule: {
       day: this.day,
       hour: this.hour
-    }
+    },
+    period: this.period
   }
 }
 
