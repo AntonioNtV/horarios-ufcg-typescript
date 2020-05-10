3f86523f6880e0e185fae1d411988f444f2e26b4
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2020-05-10 16:41:00 -0300

    Configuring Jest Mongo Test

--- __tests__/functional/import.spec.ts
+++ __tests__/functional/import.spec.ts
@@ -1,3 +1,28 @@
-test('sum two numbers', () => {
-  expect(1 + 1).toBe(2)
+import mongoose from 'mongoose'
+import Schedule from '../../src/models/Schedule'
+
+describe('Import', () => {
+  beforeAll(async () => {
+    if (!process.env.MONGO_URL) {
+      throw new Error('MongoDB server not initialized')
+    }
+
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+  })
+
+  afterAll(async () => {
+    await mongoose.connection.close()
+  })
+
+  beforeEach(async () => {
+    await Schedule.deleteMany({})
+  })
+
+  it('should import all schedule from a certain .csv file', async () => {
+    console.log('it should import all schedule from a certain .csv file')
+  })
 })
