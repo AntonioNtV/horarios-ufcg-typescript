393c24afb4b9e237c769a92e444a052c9624ea5f
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2020-05-10 21:01:47 -0300

    Updating ImportScheduleService to read from a json:

--- src/services/ImportScheduleService.ts
+++ src/services/ImportScheduleService.ts
@@ -1,10 +1,36 @@
+import SchedulesData from '../data/SchedulesData'
+import Schedule from '../models/Schedule'
+import ScheduleData from './structures/ScheduleData'
+
 class ImportScheduleService {
   public run (): void {
-    console.log('run')
+    this.importToMongo(SchedulesData.data)
   }
 
-  private exportToMongo (): void {
-    console.log('export to mongo')
+  private async importToMongo (schedules: Array<ScheduleData>): Promise<void> {
+    const formattedSchedules = schedules.map(this.parseToMongoFormat)
+
+    formattedSchedules.forEach(schedule => {
+      Schedule.create(schedule)
+    })
+  }
+
+  private parseToMongoFormat (obj: ScheduleData): object {
+    const { periodo, codigo, turma, disciplina, horarios } = obj
+
+    const period = periodo === '' ? 'N/D' : periodo
+
+    if (!horarios) {
+      console.log(horarios)
+    }
+
+    return {
+      code: codigo,
+      name: disciplina,
+      class: turma,
+      period: period,
+      schedule: horarios
+    }
   }
 }
 
