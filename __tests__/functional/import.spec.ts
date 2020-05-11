44ffc9b15b04f8c6e8d3a587739752b27a1071aa
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2020-05-10 21:02:20 -0300

    Updating import tests

--- __tests__/functional/import.spec.ts
+++ __tests__/functional/import.spec.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import Schedule from '../../src/models/Schedule'
+import ImportScheduleService from '../../src/services/ImportScheduleService'
 
 describe('Import', () => {
   beforeAll(async () => {
@@ -23,7 +24,20 @@ describe('Import', () => {
     })
   })
 
-  it('should import schedule', async () => {
-    expect(1 + 1).toBe(2)
+  it('should import schedule', () => {
+    ImportScheduleService.run()
+
+    const schedules = Schedule.find({
+    }).lean()
+
+    console.log(schedules)
+
+    expect(schedules).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          code: '1307169'
+        })
+      ])
+    )
   })
 })
